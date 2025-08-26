847a4b07c4b4e4054d0d12bc29abb9aab6cab966
Anastasia Zhabko <nastya.zhabko86@gmail.com>
2025-08-26 21:18:55 +0300

    init commit

---
+++ src/main/java/com/nastyazhabko/javacore/App.java
@@ -0,0 +1,13 @@
+package com.nastyazhabko.javacore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
